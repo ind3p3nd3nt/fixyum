28895104883e771761429d0eaac02a21a58d0ebc
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-09-12 11:14:38 -0400

    Update fixyum.sh

--- fixyum.sh
+++ fixyum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget python-iniparse-0.4-9.el7.noarch.rpm
-wget yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
-wget yum-3.4.3-158.el7.centos.noarch.rpm
-wget yum-plugin-fastestmirror-1.1.31-45.el7.noarch.rpm
+wget http://mirrors.163.com/centos/7/os/x86_ 64/Packages/python-iniparse-0.4-9.el7.noarch.rpm
+wget http://mirrors.163.com/centos/7/os/x86_ 64/Packages/yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
+wget http://mirrors.163.com/centos/7/os/x86_ 64/Packages/yum-3.4.3-158.el7.centos.noarch.rpm
+wget http://mirrors.163.com/centos/7/os/x86_ 64/Packages/yum-plugin-fastestmirror-1.1.31-45.el7.noarch.rpm
 rpm -ivh *.rpm
 yum update
